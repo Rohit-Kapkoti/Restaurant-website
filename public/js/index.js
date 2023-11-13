8aeebd48295f90ae6417f646c0a6b421db8d443f
Rohit Kapkoti <rohitkapkoti0@gmail.com>
2023-11-13 13:03:43 +0530

    fix: closing of nav by clicking outside of it

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,41 @@
-var navbar= document.querySelector(".navbar");
+var navbar = document.querySelector(".navbar");
 
-window.addEventListener('scroll',() => {
+window.addEventListener('scroll', () => {
 
-   
-    navbar.classList.toggle("sticky",window.scrollY > 0);
+
+    navbar.classList.toggle("sticky", window.scrollY > 0);
 
 })
 
 
 var burger = document.querySelector('.burger')
 var navbar = document.querySelector('.navbar')
-var max_width =document.querySelector('.nav')
+var max_width = document.querySelector('.nav')
 
 
-burger.addEventListener('click', () => {
-    navbar.classList.toggle('h_nav')
-    
-   max_width.classList.toggle('v_class')
-    
-})
+
+burger.addEventListener('click', (event) => {
+    event.stopPropagation();
+    navbar.classList.toggle('h_nav');
+    max_width.classList.toggle('v_class');
+});
+
+document.addEventListener('click', function (event) {
+    var isBurgerClick = event.target.classList.contains('burger');
+    var isNavClick = event.target.classList.contains('nav');
+
+    if (!isBurgerClick && !isNavClick) {
+        navbar.classList.add('h_nav');
+        max_width.classList.add('v_class');
+    }
+});
+
+document.addEventListener('click', function (event) {
+    var isBurgerClick = event.target.classList.contains('burger');
+    var isNavClick = event.target.classList.contains('nav');
+
+    if (!isBurgerClick && !isNavClick) {
+        navbar.classList.add('h_nav');
+        max_width.classList.add('v_class');
+    }
+});
